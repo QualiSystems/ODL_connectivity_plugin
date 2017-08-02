bce4b8b4630ea289c40e35e6e4fa60381d65fd8b
anthony <anton.p@qualisystems.com>
2017-08-02 18:15:08 +0300

    add boron version

--- impl/src/main/java/quali/impl/CloudshellProvider.java
+++ impl/src/main/java/quali/impl/CloudshellProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017 Qualisystems and others.  All rights reserved.
+ * Copyright © 2016 Qualisystems and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -8,37 +8,24 @@
 package quali.impl;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
-import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cloudshell.rev150105.CloudshellService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 public class CloudshellProvider {
 
     private static final Logger LOG = LoggerFactory.getLogger(CloudshellProvider.class);
-    private final DataBroker dataBroker;
-    private final RpcProviderRegistry rpcProviderRegistry;
-    private BindingAwareBroker.RpcRegistration<CloudshellService> serviceRegistration;
-    private final FlowProcessingService flowProcessingService;
-    private final FileRouteProcessingService fileRouteProcessingService;
 
-    public CloudshellProvider(final DataBroker dataBroker, RpcProviderRegistry rpcProviderRegistry, final FlowProcessingService flowProcessingService, final FileRouteProcessingService fileRouteProcessingService) {
+    private final DataBroker dataBroker;
+
+    public CloudshellProvider(final DataBroker dataBroker) {
         this.dataBroker = dataBroker;
-        this.rpcProviderRegistry = rpcProviderRegistry;
-        this.flowProcessingService = flowProcessingService;
-        this.fileRouteProcessingService = fileRouteProcessingService;
     }
 
     /**
      * Method called when the blueprint container is created.
      */
     public void init() {
-        serviceRegistration = rpcProviderRegistry.addRpcImplementation(CloudshellService.class,
-                new CreateRouteImpl(this.flowProcessingService, this.fileRouteProcessingService));
-
-        LOG.info("CloudshellProvider Initiated");
+        LOG.info("CloudshellProvider Session Initiated");
     }
 
     /**
