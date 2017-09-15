3520b724a54f944d38348588a65a056a03e6c6d1
Anthony Piddubny <anton.p@qualisystems.com>
2017-09-15 11:53:57 +0300

    add logs

--- impl/src/main/java/quali/impl/PacketHandler.java
+++ impl/src/main/java/quali/impl/PacketHandler.java
@@ -8,8 +8,8 @@
 package quali.impl;
 
 import java.math.BigInteger;
-import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
 import java.util.Formatter;
 import java.util.List;
 
@@ -19,6 +19,7 @@ import org.slf4j.LoggerFactory;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnectorKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingListener;
@@ -28,17 +29,9 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.vtn.rev150328.vtns.VtnKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.vtn.vbridge.rev150907.vtn.vbridge.list.Vbridge;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.vtn.vbridge.rev150907.vtn.vbridge.list.VbridgeKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.vtn.vinterface.rev150907.vtn.mappable.vinterface.list.Vinterface;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-
-
-
 import org.opendaylight.yang.gen.v1.urn.opendaylight.vtn.rev150328.vtns.Vtn;
-
 import org.opendaylight.yang.gen.v1.urn.opendaylight.vtn.rev150328.Vtns;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.vtn.types.rev150209.VnodeName;
-
-import java.util.concurrent.ExecutionException;
-
 import org.opendaylight.yang.gen.v1.urn.opendaylight.vtn.mapping.vlan.rev150907.AddVlanMapInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.vtn.mapping.vlan.rev150907.VtnVlanMapService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.l2.types.rev130827.VlanId;
@@ -73,6 +66,9 @@ public class PacketHandler implements PacketProcessingListener {
         LOG.info("PacketHandler Closed");
     }
 
+    /**
+     * Extract ethernet type from the packet payload and convert it to the hex string
+     */
     public static String extractEtherType(byte[] payload) {
         byte[] bytes = Arrays.copyOfRange(payload, 12, 14);
         Formatter formatter = new Formatter();
@@ -85,6 +81,9 @@ public class PacketHandler implements PacketProcessingListener {
         return result;
     }
 
+    /**
+     * Extract VLAN tag from the packet payload and convert it to the integer
+     */
     public static Integer extractVlan(byte[] payload) {
         byte[] bytes = Arrays.copyOfRange(payload, 14, 16);
         return new BigInteger(bytes).intValue();
@@ -96,13 +95,14 @@ public class PacketHandler implements PacketProcessingListener {
 
     @Override
     public void onPacketReceived(PacketReceived packetReceived) {
-
         String etherType = extractEtherType(packetReceived.getPayload());
 
         if (etherType.equals(ETH_TYPE_802_1Q)) {
             NodeConnectorKey ingressKey = getNodeConnectorKey(packetReceived.getIngress().getValue());
             String inPort = ingressKey.getId().getValue();
             Integer vlan = extractVlan(packetReceived.getPayload());
+
+            LOG.info(String.format("Processing frame on port %s with VLAN %d", inPort, vlan));
 
             InstanceIdentifier<Vbridge> vbridgeIdRef = InstanceIdentifier.builder(Vtns.class)
                     .child(Vtn.class, new VtnKey(new VnodeName(VTN_TRUNKS_NAME)))
@@ -115,6 +115,9 @@ public class PacketHandler implements PacketProcessingListener {
                 Optional<Vbridge> data = readTx.read(LogicalDatastoreType.OPERATIONAL, vbridgeIdRef).get();
 
                 if (data.isPresent()) {
+                    LOG.info(String.format("Trunks VTN '%s' is present. Checking whether incoming node %s " +
+                            "is a trunk or not", inPort, VTN_TRUNKS_NAME));
+
                     Vbridge vbridgeData = data.get();
                     List<Vinterface> ifaces = vbridgeData.getVinterface();
 
@@ -122,6 +125,9 @@ public class PacketHandler implements PacketProcessingListener {
                         String mappedPort = iface.getVinterfaceStatus().getMappedPort().getValue();
 
                         if (inPort.equals(mappedPort)) {
+                            LOG.info(String.format("Incoming node %s is a trunk. Adding VLAN %d to the trunks VTN '%s'",
+                                    inPort, vlan, VTN_TRUNKS_NAME));
+
                             AddVlanMapInput input = new AddVlanMapInputBuilder()
                                     .setVlanId(new VlanId(vlan))
                                     .setTenantName(VTN_TRUNKS_NAME)
